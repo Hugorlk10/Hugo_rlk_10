b6f2b3a22a02ca14268e7829ab766b44b51c8ed9
Hugo_rlk_10 <hugobarros280209@gmail.com>
2024-09-26 20:28:13 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+
+
+
+function somaAteNumero (9) {
+    let soma = 0;
+    for (let i = 1; i <= 9; i++) {
+        soma += i;
+    }
+    return soma;
+}
+
+console.log(somaAte9(10)); // Imprime 55
+]
+
+
+
+var nome = "dsad"
+
+*/ 
+ 
+alert("ola, seja bem vindo ao gamesystem") 
+
+var (operacao == 1)
